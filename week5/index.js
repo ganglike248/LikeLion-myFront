ae67e3385e0c570a833460f87f3fc4299b2fd353
rudfkr <rudfkr248@gmail.com>
2024-05-26 02:48:23 +0900

    prac 구분

---
+++ week5/index.js
@@ -0,0 +1,131 @@
+// var hi;
+
+// hi = "계명대 멋사 12기";
+// console.log(hi);
+
+// hi = "변수는 변하는 값이다";
+// console.log(hi);
+
+// const hi = "계명대 멋사 12기";
+// console.log(hi);
+
+
+// hi = "hi";
+// console.log(hi);
+
+// const a = 10;
+// const b = 20;
+// const c = 20;
+
+// if(a>b){
+//     console.log("a가 더 크다.");
+// }
+// else if(b == c){
+//     console.log("b와 c는 같다")
+// }
+// else{
+//     console.log("b가 더 작다.");
+// }
+
+
+// const num = 10;
+
+// switch(num){
+//     case 10:
+//         console.log(num);
+//     case 2:
+//         console.log(num);
+//     default:
+//         console.log("아무것도 해당 안 됨");
+// }
+
+
+// var num = prompt("값 입력");
+// console.log(num);
+
+// var check;
+// check = num % 2;
+
+// console.log("if 문");
+
+// if(check == 0){
+//     console.log("짝수");
+// }
+// else{
+//     console.log("홀수");
+// }
+
+// console.log("스위치");
+
+// switch(check){
+//     case 0:
+//         console.log("짝수");
+//         break;
+//     case 1:
+//         console.log("홀수");
+//         break;
+//     default:
+//         console.log("해당안됨");
+// }
+
+
+// for(var i = 0; i < 5; i++){
+//     console.log(i);
+// }
+
+
+// var i = 0;
+
+// while(i < 5){
+//     console.log("i = " + 1);
+//     i++;
+// }
+
+
+// function bok(hoho){
+//     console.log(hoho + "볶음밥");
+// }
+
+// bok("새우");
+// bok("김치");
+// bok("닭가슴살");
+
+
+// const sum = (a, b) => {
+//     console.log(a + b);
+// }
+
+// sum(1, 2);
+// sum(5, 5);
+
+
+// var a = 10;
+// function test(hoho){
+//     var a = 1;
+//     console.log(a);
+// }
+
+// test();
+// console.log("전역변수 확인: " + a);
+
+
+var num = prompt("값 입력");
+
+function A(n){
+    console.log("function + for");
+    for(var i = 1; i < 10; i++){
+        console.log(n + " * " + i + " = " + n * i);
+    }
+}
+
+const B = (n) => {
+    console.log("const + switch");
+    var i = 1;
+    while(i < 10){
+        console.log(n + " * " + i + " = " + n * i)
+        i++;
+    }
+}
+
+A(num);
+B(num);
